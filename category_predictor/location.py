ee0a7b924f53ce3480e8dbfb4fc76f8959831c75
Saurabh Singh <saurabh.singh21289@gmail.com>
2016-11-21 05:02:01 -0500

    Added location testing

--- category_predictor/location.py
+++
@@ -1,5 +0,0 @@
-import folium
-map_osm = folium.Map(location=[28.4595, 77.0266])
-folium.Marker([45.3288, -121.6625], popup='Mt. Hood Meadows').add_to(map_osm)
-folium.Marker([45.3311, -121.7113], popup='Timberline Lodge').add_to(map_osm)
-map_osm.save('osm.html')
